0e79b6a03536618292eb423c24f944b62caeecc2
dema121 <andrea.dematte@gmail.com>
2019-12-09 21:14:52 +0100

    Added utils script to start/stop/update service

--- service_start.sh
+++ service_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if ! test -f "service_start_save_pid.txt"; then
-  nohup gunicorn --bind 0.0.0.0:5000 app:app > my.log 2>&1 &
+  nohup gunicorn --bind 0.0.0.0:80 app:app > my.log 2>&1 &
   echo $! > service_start_save_pid.txt
 else
   echo "Service is running... stop it before starting again"
