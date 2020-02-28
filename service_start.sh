fda34cfd81cb3e3fe34ea1f5b3737c0949b4099f
dema121 <andrea.dematte@gmail.com>
2020-02-28 15:36:52 +0100

    Updated pip version

--- service_start.sh
+++ service_start.sh
@@ -2,7 +2,7 @@
 
 if ! test -f "service_start_save_pid.txt"; then
   source venv3.7/bin/activate
-  pip install -r requirements.txt
+  pip3 install -r requirements.txt
   nohup gunicorn --bind 0.0.0.0:80 app:app > my.log 2>&1 &
   echo $! > service_start_save_pid.txt
 else
