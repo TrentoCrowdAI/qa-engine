fe96d5570b3de69f9aa53c78b6733d90975dbde3
dema121 <andrea.dematte@gmail.com>
2020-02-28 16:39:09 +0100

    Separated python 2 and 3 dependencies

--- service_start.sh
+++ service_start.sh
@@ -2,7 +2,8 @@
 
 if ! test -f "service_start_save_pid.txt"; then
   source venv3.7/bin/activate
-  pip3 install -r requirements.txt
+  pip install -r requirements_python2.txt
+  pip3 install -r requirements_python3.txt
   nohup gunicorn --bind 0.0.0.0:80 app:app > my.log 2>&1 &
   echo $! > service_start_save_pid.txt
 else
